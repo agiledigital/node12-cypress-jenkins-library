7ff55e210ba60859f654f53d08baf93fa514d635
Haolin Jiang <hljiang@agiledigital.com.au>
2020-02-05 16:53:14 +1100

    Update buildNode12Component.groovy
    Fix code coverage html location.

--- vars/node12BuilderContainerTemplate.groovy
+++ vars/node12BuilderContainerTemplate.groovy
@@ -1,10 +1,12 @@
 def call() {
   return [
     containerTemplate(
-      name: 'node12-builder',
+      name: 'node12-builder-big',
       image: 'cypress/base:12.14.0',
       alwaysPullImage: true,
       command: 'cat',
+      resourceRequestCpu: '3000m',
+      resourceLimitCpu: '4000m',
       ttyEnabled: true
     )
   ]
